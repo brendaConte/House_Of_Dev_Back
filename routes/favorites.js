1319f6b64b4caf946b84224ea8b53da2727475e6
brendaConte <brendamconte@gmail.com>
2023-08-26 14:39:04 -0300

    fixing bugs

--- routes/favorites.js
+++ routes/favorites.js
@@ -8,7 +8,7 @@ const {
 const router = express.Router();
 
 router.post("/add-favorites", add_favorite);
-router.delete("/delete-favorites/:id", delete_favorite);
+router.delete("/delete-favorites/:userId/:propertyId", delete_favorite);
 router.get("/all-favorites", all_favorites);
 router.get("/by-id/:id", favorites_by_user_id);
 
